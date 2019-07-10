80b175e691754023431995cf12ba5f5b55c0f5e2
mmajewsk <maciej.w.majewski@gmail.com>
2019-07-10 20:38:35 +0200

    initial commitw

---
+++ install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+DIR1=$(pwd)/builds
+MAINDIR=$(pwd)/builds/3rdparty
+CONDA_ENV_NAME="SlumDunkEnv"
+
+# movement
+mkdir ${DIR1}
+mkdir ${MAINDIR}
+cd ${MAINDIR}
+mkdir eigen3
+
+# conda
+conda create -y -n CONDA_ENV_NAME python=3.6
+source activate CONDA_ENV_NAME
+conda install opencv -y
+conda install pytorch torchvision -c pytorch -y
+conda install -c conda-forge imageio -y
+conda install ffmpeg -c conda-forge -y
+
+# movement
+cd ${MAINDIR}
+cd eigen3
+
+wget http://bitbucket.org/eigen/eigen/get/3.3.5.tar.gz
+tar -xzf 3.3.5.tar.gz
+cd eigen-eigen-b3f3d4950030
+
+# movement
+mkdir build
+cd build
+
+
+cmake .. -DCMAKE_INSTALL_PREFIX=${MAINDIR}/eigen3_installed/
+make install
+
+# movement
+cd ${MAINDIR}
+
+wget https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.zip
+unzip glew-2.1.0.zip
+
+# movement
+cd glew-2.1.0/
+cd build
+cmake ./cmake  -DCMAKE_INSTALL_PREFIX=${MAINDIR}/glew_installed
+make -j4
+make install
+
+# movement
+cd ${MAINDIR}
+pip install numpy --upgrade
+rm Pangolin -rf
+git clone https://github.com/stevenlovegrove/Pangolin.git
+
+# movement
+cd Pangolin
+mkdir build
+cd build
+cmake .. -DCMAKE_PREFIX_PATH=${MAINDIR}/glew_installed/ -DCMAKE_LIBRARY_PATH=${MAINDIR}/glew_installed/lib/ -DCMAKE_INSTALL_PREFIX=${MAINDIR}/pangolin_installed
+cmake --build .
+
+# movement
+cd ${MAINDIR}
+rm ORB_SLAM2 -rf
+rm ORB_SLAM2-PythonBindings -rf
+git clone https://github.com/mmajewsk/ORB_SLAM2
+git clone https://github.com/mmajewsk/ORB_SLAM2-PythonBindings
+
+# movement
+cd ${MAINDIR}/ORB_SLAM2
+sed -i "s,cmake .. -DCMAKE_BUILD_TYPE=Release,cmake .. -DCMAKE_BUILD_TYPE=Release -DEIGEN3_INCLUDE_DIR=${MAINDIR}/eigen3_installed/include/eigen3/ -DCMAKE_INSTALL_PREFIX=${MAINDIR}/ORBSLAM2_installed ,g" build.sh
+ln -s ${MAINDIR}/eigen3_installed/include/eigen3/Eigen ${MAINDIR}/ORB_SLAM2/Thirdparty/g2o/g2o/core/Eigen
+./build.sh
+
+# movement
+cd build
+
+make install
+
+# movement
+cd ${MAINDIR}
+cd ORB_SLAM2-PythonBindings/src
+
+ln -s ${MAINDIR}/eigen3_installed/include/eigen3/Eigen Eigen
+
+# movement
+cd ${MAINDIR}/ORB_SLAM2-PythonBindings
+mkdir build
+cd build
+
+CONDA_DIR=$(dirname $(dirname $(which conda)))
+sed -i "s,lib/python3.5/dist-packages,${CONDA_DIR}/envs/${CONDA_ENV_NAME}/lib/python3.6/site-packages/,g" ../CMakeLists.txt
+cmake .. -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")/libpython3.6m.so -DPYTHON_EXECUTABLE:FILEPATH=`which python` -DCMAKE_LIBRARY_PATH=${MAINDIR}/ORBSLAM2_installed/lib -DCMAKE_INCLUDE_PATH=${MAINDIR}/ORBSLAM2_installed/include;${MAINDIR}/eigen3_installed/include/eigen3 -DCMAKE_INSTALL_PREFIX=${MAINDIR}/pyorbslam2_installed
+make
+make install
+cp ${MAINDIR}/ORB_SLAM2/Vocabulary/ORBvoc.txt ${DIR1}/data/
---
+++ readme.md
@@ -0,0 +1,12 @@
+
+This is an attempt of creating a guid/script that will show you how to create the environemnt for both orb slam python bindings, but osmap as well.
+This relies heavilly on a script found here:
+
+https://gist.github.com/ducha-aiki/2c29cdfd47fa4fe65f1ca083d8f09ef2
+
+Also, Im using my own forks of 
+
+https://github.com/mmajewsk/ORB_SLAM2
+https://github.com/mmajewsk/ORB_SLAM2-PythonBindings
+
+
